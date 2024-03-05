ca31f8e7ed31f6cae08f87e7b726ee170e21d355
chadboothe-md <55999308+chadboothe-md@users.noreply.github.com>
2024-03-05 16:11:01 +0000

    Nodejs baseline with Express

--- server.js
+++ server.js
@@ -6,15 +6,26 @@
 'use strict';
 
 const express = require('express');
+const app = express();
+const path = require('path');
 
 // Constants
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Define the directory where your HTML files (views) are located
+app.set('views', path.join(__dirname, 'views'));
+
+// Optionally, you can define a static files directory (CSS, JS, images, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
 // App
-const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello remote world!\n');
+	//res.send('Hello remote world!\n');
+	res.render("index");
 });
 
 app.listen(PORT, HOST);
